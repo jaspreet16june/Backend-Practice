9c044c7aed97c782ed2be76be0fbd08efe900aa3
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-05 20:45:40 +0530

    404 page

--- app1.js
+++ app1.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
 
-
+app.use(express.static("public"));
 app.use(express.json());
+
 app.listen("8000",function(){
     console.log("Server 8000 is listening");
 })
+
 let content = JSON.parse(fs.readFileSync("./data.json"));
 
 const userRouter = express.Router();
@@ -16,21 +19,26 @@ const userRouter = express.Router();
 app.use('/user',userRouter);
 // app.use('/auth',authRouter);
 
+app.use(function(req,res){
+    let rest_of_the_path = path.join("/public","404.html")
+    res.sendFile(path.join(__dirname,rest_of_the_path));
+})
+
 userRouter
-         .route("/")
+.route("/")
          .get(getUsers)
-         .post(createUser)
+         .post(bodyChecker,createUser)
 
-// authRouter
-//          .route("/")
-//          .get(authUser)
-
-// userRouter
-//          .route("/:id")
-//          .get(userId)
-
-
-function createUser(req,res){
+         // authRouter
+         //          .route("/")
+         //          .get(authUser)
+         
+         // userRouter
+         //          .route("/:id")
+         //          .get(userId)
+         
+         
+    function createUser(req,res){
     let body = req.body;
     console.log(body);
 
@@ -42,8 +50,21 @@ function createUser(req,res){
     })
 }
 
+
 function getUsers(req,res){    
-        res.status(200).json({
-            message: content
-        })
+    res.status(200).json({
+        message: content
+    })
+}
+
+function bodyChecker(req,res,next){
+    let body = req.body;
+    
+    let isPresent = Object.keys(body);
+    console.log(isPresent.length);
+    if(isPresent.length){
+        next();
+    }else{
+        res.send("send details in body")
+    }
 }
