7b07148c2e7e3bdae9d52326959f68d5aeac3e2f
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-06 17:27:22 +0530

    SignUp Function

--- app1.js
+++ app1.js
@@ -26,8 +26,11 @@ userRouter
 
 authRouter
          .route("/signup")
-         .get(bodyChecker,signUpUser)
-         
+         .post(bodyChecker,signUpUser)
+
+authRouter
+         .route("/login")
+         .post(bodyChecker,loginUser)
          // userRouter
          //          .route("/:id")
          //          .get(userId)
@@ -63,6 +66,7 @@ function bodyChecker(req,res,next){
         res.send("send details in body")
     }
 }
+
 function signUpUser(req,res){
     try{
         let {email,password,confirmPassword} = req.body
@@ -85,8 +89,36 @@ function signUpUser(req,res){
         })
     }
 }
+function loginUser(req,res){
+    try{
 
-app.use(function(req,res){
-    let rest_of_the_path = path.join("/public","404.html")
-    res.sendFile(path.join(__dirname,rest_of_the_path));
-})
+        let {email,password} = req.body;
+        let obj =  content.find((obj)=>{
+            return obj.email == email;
+        })
+        if(!obj){
+            return res.status(404).json({
+                message:"User not found"
+            })
+        }
+        if(obj.password == password){
+            res.status(200).json({
+                message:"user logged-in"
+            })
+        }
+    }
+        catch(err){
+            res.status(500).json({
+                message:err.message,
+            })
+        }
+    }
+
+
+
+
+
+// app.use(function(req,res){
+//     let rest_of_the_path = path.join("/public","404.html")
+//     res.sendFile(path.join(__dirname,rest_of_the_path));
+// })
