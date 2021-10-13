4550a23bff63ff8930ff9ee9d31611bf49d2f18d
jaspreet kaur <jaspreetkaur16june@gmail.com>
2021-10-13 18:41:29 +0530

    CRUD Operations added

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,31 +1,95 @@
 const express = require('express');
 const {protectRoute,bodyChecker} = require("./utilFun")
 const userRouter = express.Router();
+const userModel = require("../userModel")
 
 userRouter
          .route("/")
          .get(protectRoute,getUsers)
          .post(bodyChecker,createUser)
-
-
- 
-         function createUser(req,res){
-            let body = req.body;
-            console.log(body);
         
-            content.push(body);
-            fs.writeFileSync("./data.json",JSON.stringify(content));
-           
-            res.status(200).json({
-                message:content,
-            })
+        
+userRouter 
+         .route("/:id")
+         .post(bodyChecker,getUser)
+         .post(bodyChecker,deleteUser)
+         .post(bodyChecker,updateUser)
+
+ //onlu authorized to admin
+        async function createUser(req,res){
+            try{
+                let user = await userModel.create(req.body)
+                res.status(202).json({
+                    user:user
+                })
+
+            }
+            catch(err){
+                res.status(500).json({
+                    message:"server error"
+                })
+            }
         }
         
         
-        function getUsers(req,res){    
-            res.status(200).json({
-                message: content
+        async function getUser(req,res){    
+         try{
+            let {id} = req.params;
+           let user =  await userModel.findById(id);
+            res.status(202).json({
+                message: user
             })
+         }
+         catch(err){
+            res.status(500).json({
+                message:"server error"
+            })
+         }
         }
-  
+        async function getUsers(req,res){
+            try{
+              let user =   await userModel.find();
+              res.status(202).json({
+                message: user
+            })
+         }
+         catch(err){
+            res.status(500).json({
+                message:"server error"
+            })
+         }
+        }
+        async function deleteUser(req,res){
+            try{
+                let { id } = req.body
+                let user = await userModel.findByIdAndDelete(id);
+                res.status(202).json({
+                    message:"User Is Deleted"
+                })
+            }
+            catch{
+                res.status(500).json({
+                    message:"server error"
+                })
+            }
+        }
+        async function updateUser(req,res){
+            try{
+                let {id} = req.params
+                let user = await userModel.findById(id);
+              
+                    for(let key in req.body){
+                        user[key] = req.body[key]
+                    }
+                    await user.save();
+                    res.status(202).json({
+                        message:"User Is Updated"
+                    })
+            }
+            catch(err){
+                res.status(500).json({
+                    message:"servererror"
+                })
+            }
+        }
 module.exports = userRouter;
